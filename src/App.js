27dba51b311b5a113679a8426a1717680b840e36
Alex Spina <Alex.Spina@ses.com>
2018-12-16 21:16:08 -0500

    websocket connection successful but need to get apollo client working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Map from './components/Map'
 import style from './style/App/App.module.css'
+import DontReadTheComments from './components/subscriptions'
 
 function App () {
   const svgMapBounds = useState([[-90, -180.4], [89.5, 180]])
@@ -8,6 +9,7 @@ function App () {
   return (
     <div className={style.app}>
       <Map svgMapBounds={svgMapBounds} />
+      <DontReadTheComments />
     </div>
   )
 }
