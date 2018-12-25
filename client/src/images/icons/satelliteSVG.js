56e2b05d97c0fa37b5bbec9eef123e691725b170
Alex Spina <Alex.Spina@ses.com>
2018-12-25 15:36:54 -0500

    satellite svg icons added to map

---
+++ client/src/images/icons/satelliteSVG.js
@@ -0,0 +1,6 @@
+const satelliteSVG = `<svg id='satellite' viewBox='0 0 131.3 113.5' className='footer__form__button__svg'>
+<path className='footer__button__svg__path' d='M58.5,76.3l-10.3-8.2l25.2-32l10.3,8.2L58.5,76.3z M99,65.7l-17.5,3.4L82,87.5l31.3,25l17.1-21.8L99,65.7z
+        M32.3,47.8L32.3,47.8l17.5-3.4L49.4,26L18.1,1L1,22.8L32.3,47.8z M81.5,69.1l-10.7-8.3 M49.7,44.2l9.9,7.7' stroke='#ffffff' strokeWidth='4' />
+</svg>`
+
+export default satelliteSVG
